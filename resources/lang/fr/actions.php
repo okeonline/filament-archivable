ac69f5ef5f45f18da6ff08ad9bbe94867fe14cbe
OLIVIER Z. <zinsouoliviers@gmail.com>
2024-10-22 02:41:42 +0100

    Create actions.php in fr

---
+++ resources/lang/fr/actions.php
@@ -0,0 +1,65 @@
+<?php
+
+return [
+
+    'archive' => [
+
+        'single' => [
+
+            'label' => 'Archiver',
+
+            'modal' => [
+
+                'heading' => 'Archiver :label',
+
+                'actions' => [
+
+                    'archive' => [
+
+                        'label' => 'Archiver',
+                    ],
+
+                ],
+
+            ],
+
+            'notifications' => [
+
+                'archived' => [
+
+                    'title' => 'Enregistrement archivé',
+                ],
+            ],
+        ],
+    ],
+
+    'unarchive' => [
+
+        'single' => [
+
+            'label' => 'Désarchiver',
+
+            'modal' => [
+
+                'heading' => 'Désarchiver :label',
+
+                'actions' => [
+
+                    'unarchive' => [
+
+                        'label' => 'Désarchiver',
+                    ],
+
+                ],
+            ],
+
+            'notifications' => [
+
+                'unarchived' => [
+
+                    'title' => 'Enregistrement désarchivé',
+                ],
+            ],
+        ],
+    ],
+];
